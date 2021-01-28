b8d9dc303edf4d9233868aa682d9ed947182022b
moepapahlaing <moepapahlaing21@gmail.com>
2021-01-28 10:25:14 +0630

    routes and pages

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Switch, Route}  from 'react-router-dom';
+import Header from './components/nav/Header';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
